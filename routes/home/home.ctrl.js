6bf873da68deee9cfcd2afd8d649544540a4b5a0
jason191188 <mytreasure1164@gmail.com>
2023-02-10 00:39:30 +0900

    직무학습발표마무리

--- routes/home/home.ctrl.js
+++ routes/home/home.ctrl.js
@@ -17,22 +17,6 @@ const process = {
     const user = new User(req.body);
     const response = user.login();
     return res.json(response);
-    // const id = req.body.id,
-    //       pw = req.body.pw;
-    // const users = UserStorage.getUsers("id","pw");
-
-    // const response = {};
-    // if (users.id.includes(id)) {
-    //   const idx = users.id.indexOf(id);
-    //   if (users.pw[idx] === pw) {
-    //     response.success = true;
-    //     return res.json(response);
-    //   }
-    // }
-
-    // response.success= false
-    // response.msg= "로그인에 실패했습니다.";
-    // return res.json(response);
   },
 };
 
